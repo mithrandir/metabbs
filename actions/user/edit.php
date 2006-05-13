ef09238b36e5deb8a1836105aed22ea0a4902bb9
tinuviel <tinuviel@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-05-13 15:34:07 +0000

    추가 수정 -_-

--- actions/user/edit.php
+++ actions/user/edit.php
@@ -3,12 +3,9 @@ if (is_post()) {
 	$old_password = $user->password;	
 	$user->import($_POST['user']);
 	$new_password = $user->password;
-	if ($old_password == $new_password) {
+	if ($old_password != $new_password) {
 		cookie_register('password', md5($new_password));
 		$user->password = md5($new_password);
-	}
-	else {
-		$user->password = $old_password;
 	}
 	$user->save();
 	redirect_back();
