8b77a7c0e1b818d0e2e7d67768df5f8c24dd29e5
daybreaker <daybreaker@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-05-13 20:31:26 +0000

    + screenshot 수정/추가

--- skins/_admin/edit_skin.php
+++ skins/_admin/edit_skin.php
@@ -1,17 +1,17 @@
 <?php
-function print_skin_screenshot ($skin) {
+function print_skin_screenshot($skin, $class = '') {
 	global $board;
 ?>
-<a href="?action=save&amp;skin=<?=$skin?><? if ($board->exists()) { ?>&amp;board_id=<?=$board->id?><? } ?>">
+<div<?=($class == '') ? '' : " class=\"$class\""?>>
 <h4><?=$skin?></h4>
-<div>
+<a href="?action=save&amp;skin=<?=$skin?><? if ($board->exists()) { ?>&amp;board_id=<?=$board->id?><? } ?>">
 <? if(file_exists("./skins/$skin/screenshot.jpg")) { ?>
-<img src="./skins/<?=$skin?>/screenshot.jpg" style="width:320px;border:1px solid black;vertical-align:middle;" />
+	<img src="./skins/<?=$skin?>/screenshot.jpg" width="320" alt="skin screenshot" />
 <? } else { ?>
-<div style="width:320px;border:1px solid black;float:left;">No Screen Shot</div>
+	<div class="skins-noscreenshot">No Screen Shot</div>
 <? } ?>
 </a>
-<br />
+</div>
 <? } ?>
 
 <ul id="edit_section">
@@ -20,12 +20,14 @@ function print_skin_screenshot ($skin) {
     <li>Skin</li>
 </ul>
 <h2>Skin</h2>
+<div id="skins-current">
 <h3>Current Skin</h3>
 <? print_skin_screenshot($board->skin);?>
-
+</div>
+<div id="skins-available">
 <h3>Available Skins</h3>
-<p>
 <? foreach ($skins as $skin) { ?>
-<? print_skin_screenshot($skin);?>
+<? print_skin_screenshot($skin, 'skins-item');?>
 <? } ?>
-</p>
+</div>
+<div style=""></div>
