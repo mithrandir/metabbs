1f6b6a6aebc08a41af06f73e1e5b72d73fb02342
tinuviel <tinuviel@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-05-13 15:20:37 +0000

    사용자 정보 수정에서 비밀번호를 공백으로 남겨두면 이미 md5된 비밀번호를 다시 md5하던 버그 수정

--- actions/user/edit.php
+++ actions/user/edit.php
@@ -1,13 +1,15 @@
 <?php
 if (is_post()) {
-	$new_password = $_POST['user']['password'];
-	if (!$new_password) {
-		unset($_POST['user']['password']);
-	} else {
-		cookie_register('password', md5($new_password));
-	}
+	$old_password = $user->password;	
 	$user->import($_POST['user']);
-	$user->password = md5($user->password);
+	$new_password = $user->password;
+	if ($new_password) {
+		cookie_register('password', md5($new_password));
+		$user->password = md5($new_password);
+	}
+	else {
+		$user->password = $old_password;
+	}
 	$user->save();
 	redirect_back();
 }
