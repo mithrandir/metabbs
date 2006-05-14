333c6eca245c81070926dd97dd558ebf537dda2b
daybreaker <daybreaker@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-05-14 04:58:32 +0000

    * rss/atom에 htmlspecialchars 적용

--- actions/board/atom.php
+++ actions/board/atom.php
@@ -6,7 +6,7 @@ if ($board->perm_read > $user->level) {
 $page = new Page($board, 1);
 $posts = $page->get_posts();
 header("Content-Type: text/xml; charset=UTF-8");
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<?xml-stylesheet type=\"text/xsl\" href=\"$skin_dir/board/atom.xsl\"?>\n";
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
@@ -18,7 +18,7 @@ echo "<?xml-stylesheet type=\"text/xsl\" href=\"$skin_dir/board/atom.xsl\"?>\n";
 	<entry>
 		<title><![CDATA[<?=$post->title?>]]></title>
 		<id><?=full_url_for($post)?></id>
-		<content><?=$post->get_body()?></content>
+		<content><?=htmlspecialchars($post->get_body())?></content>
 		<author><name><![CDATA[<?=$post->name?>]]></name></author>
 		<updated><?=date_format("%Y-%m-%d"."T%H:%M:%SZ", $post->created_at)?></updated>
 	</entry>
--- actions/board/rss.php
+++ actions/board/rss.php
@@ -6,7 +6,7 @@ if ($board->perm_read > $user->level) {
 $page = new Page($board, 1);
 $posts = $page->get_posts();
 header("Content-Type: text/xml; charset=UTF-8");
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<?xml-stylesheet type=\"text/xsl\" href=\"$skin_dir/board/rss.xsl\"?>\n";
 ?>
 <rss version="2.0">
@@ -18,7 +18,7 @@ echo "<?xml-stylesheet type=\"text/xsl\" href=\"$skin_dir/board/rss.xsl\"?>\n";
 		<item>
 			<title><![CDATA[<?=$post->title?>]]></title>
 			<link><?=full_url_for($post)?></link>
-			<description><?=$post->get_body()?></description>
+			<description><?=htmlspecialchars($post->get_body())?></description>
 			<author><![CDATA[<?=$post->name?>]]></author>
 			<pubDate><?=date_format("%d %b %Y %H:%M:%S", $post->created_at)?></pubDate>
 		</item>
