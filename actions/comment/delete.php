4eee8f8a52ec071fdda90b33755eac57c0a2ce2d
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-06-17 11:37:40 +0000

    admindelete.php 다시 제거

--- actions/comment/delete.php
+++ actions/comment/delete.php
@@ -4,12 +4,15 @@ if (is_post() && ($user->level >= $board->perm_delete ||
 		$comment->password == md5($_POST['password']))) {
 	$comment->delete();
 	redirect_to(url_for($post));
-}
-else if ($comment->user_id != 0 && $comment->user_id == $user->id ||
+} else {
+	$nav[] = link_to(i("Cancel"), $post);
+
+	if ($comment->user_id != 0 && $comment->user_id == $user->id ||
 		$user->level >= $board->perm_delete) {
-	render('admindelete');
-}
-else if ($comment->user_id == 0) {
+		$ask_password = false;
+	} else {
+		$ask_password = true;
+	}
 	render('delete');
 }
 ?>
