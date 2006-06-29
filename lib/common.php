dc4bb960b6500d1d0340a476f3b392a654604df5
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-06-29 03:42:51 +0000

    외부에서 common.php를 불러와도 작동

--- lib/common.php
+++ lib/common.php
@@ -9,10 +9,14 @@ function get_base_path() {
 	return $path;
 }
 
-if (!file_exists('metabbs.conf.php')) {
+$metabbs_dir = realpath(dirname(__FILE__) . '/..');
+
+if (!file_exists($metabbs_dir . '/metabbs.conf.php')) {
 	echo '<h1>Config file not found.</h1><p>Did you install metaBBS? :) <a href="'.get_base_path().'install.php">Go to install page &raquo;</a></p>';
 	exit;
 }
+
+ini_set("include_path", ini_get("include_path") . ';' . $metabbs_dir);
 
 require_once("core.php");
 require_once("request.php");
