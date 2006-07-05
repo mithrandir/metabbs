dc401da7177a69569002b8b97c728fedfbf13e71
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-07-05 10:07:07 +0000

    workaround for CGI environment

--- lib/common.php
+++ lib/common.php
@@ -13,6 +13,10 @@ function get_base_path() {
 
 $metabbs_dir = realpath(dirname(__FILE__) . '/..');
 
+if (!isset($_SERVER['REQUEST_URI'])) { // workaround for CGI environment
+	$_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'];
+}
+
 if (!file_exists($metabbs_dir . '/metabbs.conf.php')) {
 	echo '<h1>Config file not found.</h1><p>Did you install metaBBS? :) <a href="'.get_base_path().'install.php">Go to install page &raquo;</a></p>';
 	exit;
