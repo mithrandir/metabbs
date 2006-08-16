1886c960f430aef1d982045324f07e59f50ca25a
daybreaker <daybreaker@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-08-16 16:26:43 +0000

    사용자 삭제 및 정보 조회 기능 레이아웃

--- skins/_admin/users.php
+++ skins/_admin/users.php
@@ -5,16 +5,18 @@
 	<th><input type="checkbox" value="1" onchange="checkAll(this.form, this.checked)" /></th>
 	<th class="name"><?=i('Name')?></th>
 	<th class="level"><?=i('Level')?></th>
+	<th class="actions"><?=i('Actions')?></th>
 </tr>
 <? foreach ($users as $user) { ?>
 <tr>
 	<td><input type="checkbox" name="user_id[<?=$user->id?>]" class="check" /></td>
 	<td class="name"><?=$user->name?> <small>(<?=$user->user?>)</small></td>
 	<td class="level"><?=$user->level?></td>
+	<td class="actions"><?=link_text('#', i('View'))?> | <?=link_text('#', i('Delete user'))?></td>
 </tr>
 <? } ?>
 </table>
 
-<h2><?=i('Change Level')?></h2>
+<h2><?=i('Mass operation')?></h2>
 <p><?=i('Change levels of selected users to %s', '<input type="text" name="level" value="1" size="3" />')?> (0~255) <input type="submit" value="OK" /></p>
 </form>
