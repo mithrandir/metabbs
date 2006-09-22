8c404e32c33df3fd7d4731b220d7b998a614b49e
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-09-22 08:23:03 +0000

    #63: Accept-Language 헤더로 접속자 언어 판별

--- lib/i18n.php
+++ lib/i18n.php
@@ -5,19 +5,35 @@ function i() {
 	return vsprintf($lang->get(array_shift($args)), $args);
 }
 
+class DefaultLanguage {
+	function get($text) {
+		return $text;
+	}
+}
 class I18N extends Config {
 	function I18N($lang) {
 		$this->Config("lang/$lang.php");
 		$this->lang = $lang;
 	}
 	function get($text) {
-		if ($this->lang == 'en') {
-			return $text;
+		return Config::get($text, $text);
+	}
+	function import($lang) {
+		if ($lang == 'en') {
+			return new DefaultLanguage;
+		} else if (file_exists('lang/'.$lang.'.php')) {
+			return new I18N($lang);
 		} else {
-			return Config::get($text, $text);
+			return false;
 		}
 	}
 }
 
-$lang = new I18N('ko'); //XXX
+$langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+foreach ($langs as $langcode) {
+	list($langcode) = explode(';', $langcode, 2);
+	if ($lang = I18N::import($langcode))
+		break;
+}
+if (!$lang) $lang = new DefaultLanguage;
 ?>
