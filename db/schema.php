9b3b11fac918933128fb123d65d32a9a4d24c60c
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-10-07 12:19:09 +0000

    스키마 업데이트!

--- db/schema.php
+++ db/schema.php
@@ -67,4 +67,11 @@ $t->column('board_id', 'integer');
 $t->column('name', 'string', 45);
 $t->add_index('board_id');
 $conn->add_table($t);
+
+$t = new Table('plugin');
+$t->column('name', 'string', 45);
+$t->column('enabled', 'integer', 1);
+$t->add_index('name');
+$t->add_index('enabled');
+$conn->add_table($t);
 ?>
