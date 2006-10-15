a90560b7bfe89e8e32dc78e93a94d50b9a4ec9fd
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-10-15 10:52:49 +0000

    디자인 수정

--- skins/default/edit_comment.php
+++ skins/default/edit_comment.php
@@ -1,10 +1,8 @@
 <h2><?=i('Edit Comment')?></h2>
 <form method="post">
-<p>
-	<? if ($ask_password) { ?>
-	<?=i("Password")?>: <input type="password" name="password" />
-	<? } ?>
-	<textarea name="body" rows="5" cols="50"><?=$comment->body?></textarea>
-</p>
+<? if ($ask_password) { ?>
+<p><?=i("Password")?>: <input type="password" name="password" /></p>
+<? } ?>
+<p><textarea name="body" rows="5" cols="50"><?=$comment->body?></textarea></p>
 <p><?=submit_tag(i("Edit"))?></p>
 </form>
