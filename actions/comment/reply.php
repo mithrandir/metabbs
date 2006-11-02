24d29da213628e40c0ae0a3b63d581cabe32dff8
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-11-02 07:17:55 +0000

    oops;

---
+++ actions/comment/reply.php
@@ -0,0 +1,23 @@
+<?php
+if ($account->level < $board->perm_comment) {
+	access_denied();
+}
+if (is_post()) {
+	$_comment = new Comment($_POST['comment']);
+	$_comment->user_id = $account->id;
+	$_comment->parent = $comment->id;
+	if (!$account->is_guest()) {
+		$_comment->name = $account->name;
+	} else {
+		cookie_register('name', $_comment->name);
+	}
+
+	apply_filters('PostComment', $comment);
+
+	$post = $comment->get_post();
+	$post->add_comment($_comment);
+	redirect_to(url_for($post));
+} else {
+	render('reply');
+}
+?>
