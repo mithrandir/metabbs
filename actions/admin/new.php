6329025f0c6d11d05de960875fec42bf310b6f65
cn <cn@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-11-06 15:46:40 +0000

    비어있는 게시판 명 생성되지 않게

--- actions/admin/new.php
+++ actions/admin/new.php
@@ -1,11 +1,17 @@
 <?php
-$board = new Board(array('name' => $_POST['name']));
-if ($board->validate()) {
-	$board->create();
-	redirect_to(url_for('admin'));
-} else {
-	$flash = "Board '$board->name' already exists.";
+if (empty($_POST['name'])) {
+	$flash = "Empty board's name.";
 	$boards = Board::find_all();
 	render('index');
+} else {
+	$board = new Board(array('name' => $_POST['name']));
+	if ($board->validate()) {
+		$board->create();
+		redirect_to(url_for('admin'));
+	} else {
+		$flash = "Board '$board->name' already exists.";
+		$boards = Board::find_all();
+		render('index');
+	}
 }
 ?>
