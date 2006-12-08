3d427e3c110e0588513562ce3986bfedfbddda50
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-12-08 23:24:17 +0000

    * Wiki, FrontPage 플러그인 삭제 * get_upload_size_limit()이 upload_max_filesize 리턴하도록 고침

--- lib/request.php
+++ lib/request.php
@@ -6,7 +6,8 @@ function get_requested_page() {
 	return isset($_GET['page']) ? $_GET['page'] : 1;
 }
 function get_upload_size_limit() {
-	return min(ini_get('post_max_size'), ini_get('upload_max_filesize'));
+	//return min(ini_get('post_max_size'), ini_get('upload_max_filesize'));
+	return ini_get('upload_max_filesize');
 }
 
 if (@ini_get('register_globals')) {
