ab2902cca291a0666df7c1e21c9af46cebc9c036
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-12-15 04:20:16 +0000

    블로그 스킨을 위해서 PostList 필터에서도 글 내용 처리

--- plugins/_base.php
+++ plugins/_base.php
@@ -2,6 +2,7 @@
 function format_body(&$model) {
 	$model->body = format_plain($model->body);
 }
+add_filter('PostList', 'format_body', 500);
 add_filter('PostView', 'format_body', 500);
 add_filter('PostViewComment', 'format_body', 500);
 ?>
