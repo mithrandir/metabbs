cbe23340af5b111ba2d0087a9413667a379bd7d1
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2006-12-15 04:33:32 +0000

    함수를 클래스에 집어넣음

--- plugins/Emoticon.php
+++ plugins/Emoticon.php
@@ -1,36 +1,33 @@
 <?php
-$emote_mapping = array(
-	'O:-)' => 'face-angel.png',
-	':)' => 'face-smile.png',
-	':-)' => 'face-smile.png',
-	':(' => 'face-sad.png',
-	':-(' => 'face-sad.png',
-	':|' => 'face-plain.png',
-	':-|' => 'face-plain.png',
-	';)' => 'face-wink.png',
-	';-)' => 'face-wink.png',
-	';\'-(' => 'face-crying.png',
-	';\'(' => 'face-crying.png',
-	':-D' => 'face-grin.png',
-	':D' => 'face-grin.png',
-	':-O' => 'face-surprise.png',
-	':O' => 'face-surprise.png',
-	':evil:' => 'face-devil-grin.png',
-	'8-|' => 'face-glasses.png'
-);
-
-function emote_filter(&$model) {
-	global $emote_mapping;
-	foreach ($emote_mapping as $k => $v) {
-		$model->body = str_replace($k, image_tag(METABBS_BASE_PATH.'plugins/emotes/'.$v), $model->body);
-	}
-}
-
 class Emoticon extends Plugin {
 	var $description = 'Replace emoticons to images';
+	var $mapping = array(
+		'O:-)' => 'face-angel.png',
+		':)' => 'face-smile.png',
+		':-)' => 'face-smile.png',
+		':(' => 'face-sad.png',
+		':-(' => 'face-sad.png',
+		':|' => 'face-plain.png',
+		':-|' => 'face-plain.png',
+		';)' => 'face-wink.png',
+		';-)' => 'face-wink.png',
+		';\'-(' => 'face-crying.png',
+		';\'(' => 'face-crying.png',
+		':-D' => 'face-grin.png',
+		':D' => 'face-grin.png',
+		':-O' => 'face-surprise.png',
+		':O' => 'face-surprise.png',
+		':evil:' => 'face-devil-grin.png',
+		'8-|' => 'face-glasses.png'
+	);
 	function on_init() {
-		add_filter('PostView', 'emote_filter', 1000);
-		add_filter('PostViewComment', 'emote_filter', 1000);
+		add_filter('PostView', array(&$this, 'emote_filter'), 1000);
+		add_filter('PostViewComment', array(&$this, 'emote_filter'), 1000);
+	}
+	function emote_filter(&$model) {
+		foreach ($this->mapping as $k => $v) {
+			$model->body = str_replace($k, image_tag(METABBS_BASE_PATH.'plugins/emotes/'.$v), $model->body);
+		}
 	}
 }
 
--- plugins/EnglishPatient.php
+++ plugins/EnglishPatient.php
@@ -1,37 +1,34 @@
 <?php
-$english_patient_regex = '/^[\\x{00}-\\x{7F}]+$/u';
-function english_patient_tb_filter($model) {
-	global $english_patient_regex;
-	if (preg_match($english_patient_regex, $model->title) && preg_match($english_patient_regex, $model->excerpt)) {
-		$model->valid = false;
-	}
-}
-function english_patient_cmt_filter($model) {
-	global $english_patient_regex;
-	if (strlen($model->body) > 10 && preg_match($english_patient_regex, $model->body)) {
-		echo "<h2>English Patient plugin enabled!</h2>";
-		echo "you can't post a comment only with ascii characters.";
-		exit;
-	}
-}
-function english_patient_post_filter($model) {
-	global $english_patient_regex;
-	if (preg_match($english_patient_regex, $model->title) &&
-		preg_match($english_patient_regex, $model->body)) {
-		echo "<h2>English Patient plugin enabled!</h2>";
-		echo "you can't post a comment only with ascii characters.";
-		exit;
-	}
-}
-
 class EnglishPatient extends Plugin {
 	var $plugin_name = '영어 환자';
 	var $description = 'ASCII 문자로만 작성된 글/댓글/트랙백을 막습니다.'; 
+	var $pattern = '/^[\\x{00}-\\x{7F}]+$/u';
 	function on_init() {
-		add_filter('PostTrackback', 'english_patient_tb_filter', 10);
-		add_filter('PostComment', 'english_patient_cmt_filter', 20);
-		add_filter('PostSave', 'english_patient_post_filter', 20);
+		add_filter('PostTrackback', array(&$this, 'tb_filter'), 10);
+		add_filter('PostComment', array(&$this, 'cmt_filter'), 20);
+		add_filter('PostSave', array(&$this, 'post_filter'), 20);
+	}
+	function tb_filter($model) {
+		if (preg_match($this->pattern, $model->title) && preg_match($this->pattern, $model->excerpt)) {
+			$model->valid = false;
+		}
+	}
+	function cmt_filter($model) {
+		if (strlen($model->body) > 10 && preg_match($this->pattern, $model->body)) {
+			echo "<h2>English Patient plugin enabled!</h2>";
+			echo "you can't post a comment only with ascii characters.";
+			exit;
+		}
+	}
+	function post_filter($model) {
+		if (preg_match($this->pattern, $model->title) &&
+			preg_match($this->pattern, $model->body)) {
+			echo "<h2>English Patient plugin enabled!</h2>";
+			echo "you can't post a comment only with ascii characters.";
+			exit;
+		}
 	}
 }
+
 register_plugin('EnglishPatient');
 ?>
