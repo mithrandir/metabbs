9cd6b3fff1ad36aa8e8193cfb8986ed06048c303
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-01-16 02:47:46 +0000

    스타일 미리보기 이미지 추가

--- stylish/skins/_admin/edit_skin.php
+++ stylish/skins/_admin/edit_skin.php
@@ -2,14 +2,17 @@
 <div id="skin">
 <table>
 <? foreach ($styles as $info) { list($style, $name, $creator, $license) = $info; ?>
-<tr>
+<tr<? if ($board->style == $style) { ?> class="current"<? } ?>>
+	<td><input type="radio" name="board[style]" value="<?=$style?>" <? if ($board->style == $style) { ?>checked="checked"<? } ?> onchange="this.form.submit()" /></td>
 	<td>
-	<input type="radio" name="board[style]" value="<?=$style?>" <? if ($board->style == $style) { ?>checked="checked"<? } ?> onchange="this.form.submit()" />
+	<? if (file_exists('styles/'.$style.'/preview.png')) { ?>
+	<img src="<?=METABBS_BASE_PATH?>styles/<?=$style?>/preview.png" alt="Preview" />
+	<? } ?>
 	</td>
 	<td>
 		<span class="style-name"><?=$name?></span><br />
 		<span class="creator">Created by <?=htmlspecialchars($creator)?></cite><br />
-		<span class="engine">Licensed under <?=$license_mapping[$license]?></span>
+		<span class="engine">Licensed under <?=array_key_exists($license, $license_mapping)?$license_mapping[$license]:htmlspecialchars($license)?></span>
 	</td>
 </tr>
 <? } ?>
