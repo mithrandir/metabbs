db40d85230bbcea164885b36864f16801d3f9160
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-01-17 03:09:43 +0000

    갤러리 스킨 및 스타일 추가

---
+++ skins/gallery/list.php
@@ -0,0 +1,30 @@
+<h1><?=$title?></h1>
+
+<? if ($board->use_category) { ?>
+<div id="category">
+<?=i('Category')?>:
+<a href="?"><?=i('All')?></a> (<?=$board->get_post_count()?>)
+<? foreach ($categories as $_category) { ?>
+ <a href="?search[category]=<?=$_category->id?>"><?=$_category->name?></a> (<?=$_category->get_post_count()?>)
+<? } ?>
+</div>
+<? } ?>
+
+<ul id="gallery">
+<? foreach ($posts as $post) { $attachments = $post->get_attachments(); ?>
+	<li>
+	<? if ($attachments) { ?>
+	<a href="<?=url_for($post)?>" class="thumbnail"><img src="<?=url_for($attachments[0])?>" alt="<?=$post->title?>" /></a>
+	<? } ?>
+	<? if ($board->use_category && $post->category_id) { ?><span class="category">[<?=link_to_category($post->get_category())?>]</span><? } ?><span class="title"><?=link_to($post->title, $post)?></span>
+	<? if ($count = $post->get_comment_count()) { ?>
+	<span class="comments-count">*<?=$count?></span>
+	<? } ?>
+	<br /><span class="writer"><?=$post->name?></span>
+	</li>
+<? } ?>
+</ul>
+
+<div id="gallery-footer">
+<a href="<?=$link_new_post?>"><?=i('New Post')?></a>
+</div>
