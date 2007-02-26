a5efa58b478a320f9236a14d8787f48cfb674478
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-02-26 07:53:52 +0000

    문서 파일 옮김

--- app/views/default_header.php
+++ app/views/default_header.php
@@ -8,7 +8,11 @@
 <? } ?>
 	<link rel="stylesheet" href="<?=$style_dir?>/style.css" type="text/css" />
 	<script type="text/javascript" src="<?=METABBS_BASE_PATH?>app/views/prototype.js"></script>
+<? if ($skin[0] == '_') { ?>
+	<script type="text/javascript" src="<?=METABBS_BASE_PATH?>skins/_admin/script.js"></script>
+<? } else { ?>
 	<script type="text/javascript" src="<?=METABBS_BASE_PATH?>app/views/script.js"></script>
+<? } ?>
 <? if (isset($board) && $skin[0] != '_') { ?>
 	<link rel="alternate" href="<?=url_for($board, 'rss')?>" type="application/rss+xml" title="RSS" /> 
 <? } ?>
