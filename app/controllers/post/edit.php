f906163a69f3fad77ee31469f548126619a9827e
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-02-28 07:07:08 +0000

    #124: 비밀글 수정 안되는 버그 수정

--- app/controllers/post/edit.php
+++ app/controllers/post/edit.php
@@ -6,6 +6,8 @@ if ($post->secret) {
 	if ($post->user_id != $account->id && !$account->is_admin()) {
 		access_denied();
 	} else if ($post->user_id == 0 && $account->is_guest()) {
+		if (isset($_POST['post']))
+			$_POST['password'] = $_POST['post']['password'];
 		if (is_post() && md5($_POST['password']) == $post->password) {
 		} else {
 			$action = 'secret';
@@ -13,7 +15,7 @@ if ($post->secret) {
 		}
 	}
 }
-if (is_post() && !isset($_POST['password']) && ($post->user_id != 0 && $account->id == $post->user_id || $account->level >= $board->perm_delete || $post->user_id == 0 && md5($_POST['post']['password']) == $post->password)) {
+if (is_post() && ($post->user_id != 0 && $account->id == $post->user_id || $account->level >= $board->perm_delete || $post->user_id == 0 && md5($_POST['post']['password']) == $post->password)) {
 	unset($_POST['post']['password']);
 	$post->import($_POST['post']);
 	$post->edited_at = time();
