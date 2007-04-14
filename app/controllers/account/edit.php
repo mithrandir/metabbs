bfc141c0f0e532f2f0f2f32a974a1354e6e66cd5
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-04-14 10:03:47 +0000

    암호 바뀌어도 쿠키 굽지 않음 (인증 방식 변경)

--- app/controllers/account/edit.php
+++ app/controllers/account/edit.php
@@ -2,14 +2,10 @@
 if (is_post()) {
 	$old_password = $account->password;	
 	$account->import($_POST['user']);
-	$new_password = $account->password;
-	if ($new_password) {
-		cookie_register('password', md5($new_password));
-		$account->password = md5($new_password);
-	}
-	else {
+	if (!$account->password)
 		$account->password = $old_password;
-	}
+	else
+		$account->password = md5($account->password);
 	$account->update();
 	redirect_back();
 }
