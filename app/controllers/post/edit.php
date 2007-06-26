9a148f11571c9422b0534b9472f6f40c4cbcd96f
dahlia <dahlia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-06-26 14:02:53 +0000

    #136 (....;;;;)

--- app/controllers/post/edit.php
+++ app/controllers/post/edit.php
@@ -17,6 +17,9 @@ if ($post->secret) {
 }
 if (is_post() && isset($_POST['post']) && ($post->user_id != 0 && $account->id == $post->user_id || $account->level >= $board->perm_delete || $post->user_id == 0 && md5($_POST['post']['password']) == $post->password)) {
 	unset($_POST['post']['password']);
+	if (!$account->is_admin()) {
+		unset($_POST['post']['notice']);
+	}
 	$post->import($_POST['post']);
 	$post->edited_at = time();
 	$post->edited_by = $account->id;
