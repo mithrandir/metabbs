4ec9e6e0033d23577154241bfe2387491d156dce
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-08-15 06:46:01 +0000

    사소한 버그 수정

--- db/schema.php
+++ db/schema.php
@@ -22,6 +22,7 @@ function run($conn) {
 	$t->column('user_id', 'integer');
 	$t->add_index('board_id');
 	$conn->add_table($t);
+	$conn->query("INSERT INTO ".get_table_name('board_admin')." (board_id, user_id) VALUES(0, 0)"); // insert dummy data
 
 	$t = new Table('post');
 	$t->column('board_id', 'integer');
