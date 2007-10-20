32beb6141db93f1175cc72a0a5c38176425b9764
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-10-20 11:49:40 +0000

    취소 링크 추가

--- reference-skin/delete.php
+++ reference-skin/delete.php
@@ -4,5 +4,5 @@
 <? if ($ask_password): ?>
 <p>암호: <input type="password" name="password" /></p>
 <? endif; ?>
-<p><input type="submit" value="지우기" /></p>
+<p><input type="submit" value="지우기" /> <a href="<?=$link_cancel?>">취소</a></p>
 </form>
