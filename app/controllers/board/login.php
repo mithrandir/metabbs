9cbfc98514d22ebc846515866665fd65b2315af0
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-10-20 09:58:21 +0000

    board/login 개선

--- app/controllers/board/login.php
+++ app/controllers/board/login.php
@@ -1,5 +1,10 @@
 <?php
-$controller = 'account';
-$action = 'login';
-include 'app/controllers/account/login.php';
+if (is_post()) {
+	$controller = 'account';
+	$action = 'login';
+	include 'app/controllers/account/login.php';
+} else {
+	$template = get_template($board, 'login');
+	$template->set('board', $board);
+}
 ?>
