67e5bd0ff8209159b6013a4dfde3b5bd465231fa
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-10-20 10:35:12 +0000

    delete 템플릿

--- metabbs/app/controllers/comment/delete.php
+++ metabbs/app/controllers/comment/delete.php
@@ -1,17 +1,21 @@
 <?php
-permission_required('delete', $comment);
+if (!$account->has_perm('delete', $comment))
+	access_denied();
 
 $post = $comment->get_post();
 if (is_post()) {
-	if ($account->is_guest())
-		$comment->deleted_by = $comment->name;
-	else
-		$comment->deleted_by = $account->name;
-	$comment->delete();
-	redirect_to(url_for($post));
-} else {
-	$template = get_template($board, 'delete');
-	$template->set('ask_password', false);
-	$template->set('link_cancel', url_for($post));
+	if (!$account->is_guest() || $comment->password == md5($_POST['password'])) {
+		if ($account->is_guest())
+			$comment->deleted_by = $comment->name;
+		else
+			$comment->deleted_by = $account->name;
+		$comment->delete();
+		redirect_to(url_for($post));
+	}
 }
+
+$template = get_template($board, 'delete');
+$template->set('board', $board);
+$template->set('ask_password', $account->is_guest());
+$template->set('link_cancel', url_for($post));
 ?>
--- metabbs/app/controllers/post/delete.php
+++ metabbs/app/controllers/post/delete.php
@@ -1,22 +1,26 @@
 <?php
-permission_required('delete', $post);
+if (!$account->has_perm('delete', $post))
+	access_denied();
 
 if (is_post()) {
-	apply_filters('PostDelete', $post);
+	if (!$account->is_guest() || $post->password == md5($_POST['password'])) {
+		apply_filters('PostDelete', $post);
 
-	$attachments = $post->get_attachments();
-	foreach ($attachments as $attachment) {
-		@unlink($attachment->get_filename());
-		if (file_exists('data/thumb/'.$attachment->id.'.png')) {
-			@unlink('data/thumb/'.$attachment->id.'.png');
+		$attachments = $post->get_attachments();
+		foreach ($attachments as $attachment) {
+			@unlink($attachment->get_filename());
+			if (file_exists('data/thumb/'.$attachment->id.'.png')) {
+				@unlink('data/thumb/'.$attachment->id.'.png');
+			}
+			$attachment->delete();
 		}
-		$attachment->delete();
+		$post->delete();
+		redirect_to(url_for($board));
 	}
-	$post->delete();
-	redirect_to(url_for($board));
-} else {
-	$template = get_template($board, 'delete');
-	$template->set('ask_password', false);
-	$template->set('link_cancel', url_for($post));
 }
+
+$template = get_template($board, 'delete');
+$template->set('board', $board);
+$template->set('ask_password', $account->is_guest());
+$template->set('link_cancel', url_for($post));
 ?>
