c9eb18dbd8d8bdb71e87723cc9eebd6923c38daf
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-12-15 09:46:14 +0000

    누락된 파일 추가

---
+++ tests/fixtures/data.php
@@ -0,0 +1,16 @@
+<?php
+$board_fixture = array(
+	'test' => array(
+		'id' => 1,
+		'name' => 'test'
+	),
+	'second' => array(
+		'id' => 2,
+		'name' => 'second'
+	)
+);
+
+$fixtures = array(
+	'board' => $board_fixture
+);
+?>
