5ac052aeae1d26cfd008132d9664b0cf79de346c
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2007-12-30 13:46:43 +0000

    메타데이터 테스트 추가

---
+++ metabbs/tests/post_meta_test.php
@@ -0,0 +1,35 @@
+<?php
+require_once "../lib/finder.php";
+require_once "../lib/model.php";
+require_once "../app/models/post.php";
+require_once "../app/models/post_meta.php";
+
+class PostMetaTest extends UnitTestCase {
+	function setUp() {
+		$this->metadata = new PostMeta(Post::find(5));
+	}
+
+	function testLoad() {
+		$this->metadata->load();
+		$this->assertEqual('bar', $this->metadata->attributes['foo']);
+	}
+	
+	function testGet() {
+		$this->assertEqual('', $this->metadata->get('foo'));
+
+		$this->metadata->load();
+		$this->assertEqual('bar', $this->metadata->get('foo'));
+	}
+
+	function testSet() {
+		$this->metadata->set('edited', 'no');
+		$this->assertEqual('no', $this->metadata->get('edited'));
+		$this->metadata->set('edited', 'yes');
+		$this->assertEqual('yes', $this->metadata->get('edited'));
+	}
+
+	function testReset() {
+		$this->metadata->reset();
+		$this->assertEqual('', $this->metadata->get('foo'));
+	}
+}
