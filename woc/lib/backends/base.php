68a2262f7ec3a2220f0c777be22244ac15756dc3
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-01-13 06:03:16 +0000

    코딩 스타일 맞춤

--- woc/lib/backends/base.php
+++ woc/lib/backends/base.php
@@ -1,34 +1,34 @@
 <?php 
 class BaseConnection {
 	function BaseConnection(){
-	if(get_class($this)=='BaseConnection')
-		trigger_error('Implement BaseConnection class');
+		if(get_class($this) == 'BaseConnection')
+			trigger_error('Not implemented yet');
 	}
 	function open($info){	
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function close(){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function execute($query){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function query($query){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function last_insert_id($sequence_name){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
-	function bind_params($query,$params){
-		trigger_error('Implement BaseConnection class');
+	function bind_params($query, $params){
+		trigger_error('Not implemented yet');
 	}
 	function escape($string){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function quote($value){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 	function quote_identifier($id){
-		trigger_error('Implement BaseConnection class');
+		trigger_error('Not implemented yet');
 	}
 }
