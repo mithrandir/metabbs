86b10eadd113faba0897fd023cc91025b7a10e13
lagendia <lagendia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-01-23 15:08:21 +0000

    BaseResultSet을 base.php에 stub으로 구현. mysql-backend에서 _q 함수를 bind_params로 바꾸고 인수 명 data를 params로 고침.

--- lib/backends/base.php
+++ lib/backends/base.php
@@ -1,34 +1,46 @@
 <?php 
 class BaseConnection {
-	function BaseConnection(){
+	function BaseConnection() {
 		if(get_class($this) == 'BaseConnection')
 			trigger_error('Not implemented yet');
 	}
-	function open($info){	
+	function open($info) {	
 		trigger_error('Not implemented yet');
 	}
-	function close(){
+	function close() {
 		trigger_error('Not implemented yet');
 	}
-	function execute($query){
+	function execute($query) {
 		trigger_error('Not implemented yet');
 	}
-	function query($query){
+	function query($query) { 
 		trigger_error('Not implemented yet');
 	}
-	function last_insert_id($sequence_name){
+	function last_insert_id($sequence_name) {
 		trigger_error('Not implemented yet');
 	}
-	function bind_params($query, $params){
+	function bind_params($query, $params) {
 		trigger_error('Not implemented yet');
 	}
-	function escape($string){
+	function escape($string) {
 		trigger_error('Not implemented yet');
 	}
-	function quote($value){
+	function quote($value) {
 		trigger_error('Not implemented yet');
 	}
-	function quote_identifier($id){
+	function quote_identifier($id) {
+		trigger_error('Not implemented yet');
+	}
+}
+
+class BaseResultSet {
+	function fetch() {
+		trigger_error('Not implemented yet');
+	}
+	function fetch_column() {
+		trigger_error('Not implemented yet');
+	}
+	function count() {
 		trigger_error('Not implemented yet');
 	}
 }
