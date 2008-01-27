61a954679968e0364e8e663655eb431afc88e7fc
lagendia <lagendia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-01-27 16:27:29 +0000

    mysql에서 넘어온 내용 중, sqlite에 적합하지 않은 부분을 sqlite installer에서 주석 처리

--- lib/backends/sqlite/installer.php
+++ lib/backends/sqlite/installer.php
@@ -54,9 +54,9 @@ class SQLExporter {
 	}
 }
 function db_info_form() {
-	field('host', 'Hostname', 'localhost', 'text', 'Host이름을 입력합니다. 대부분 localhost입니다.');
-	field('user', 'User ID', 'root', 'text', 'Database 사용자의 아이디를 입력합니다.');
-	field('password', 'Password', '', 'password', 'Database의 비밀번호를 입력합니다.');
+	//field('host', 'Hostname', 'localhost', 'text', 'Host이름을 입력합니다. 대부분 localhost입니다.');
+	//field('user', 'User ID', 'root', 'text', 'Database 사용자의 아이디를 입력합니다.');
+	//field('password', 'Password', '', 'password', 'Database의 비밀번호를 입력합니다.');
 	field('dbname', 'DB name', '', 'text', 'Database 이름을 입력합니다.');
 }
 function is_supported() {
