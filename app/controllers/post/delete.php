5428ea8fde4b2c51e56c844520a0ad6992397b07
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-20 10:18:42 +0000

    [1128] 계속

--- app/controllers/post/delete.php
+++ app/controllers/post/delete.php
@@ -18,9 +18,3 @@ if (is_post()) {
 		redirect_to(url_for($board));
 	}
 }
-
-$template = get_template($board, 'delete');
-$template->set('board', $board);
-$template->set('ask_password', $account->is_guest());
-$template->set('link_cancel', url_for($post));
-?>
---
+++ app/views/post/edit.php
@@ -0,0 +1,8 @@
+<?php
+$template = get_template($board, 'write');
+$template->set('board', $board);
+$template->set('post', $post);
+$template->set('extra_attributes', $extra_attributes);
+$template->set('link_list', url_for($board));
+$template->set('link_cancel', url_for($post));
+$template->render();
