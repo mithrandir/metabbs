a0dc4ca7f5138aec79429c432d563f50603b5a78
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-21 08:59:21 +0000

    오류 페이지에 버전 정보 표시

--- metabbs/themes/default/error.php
+++ metabbs/themes/default/error.php
@@ -5,18 +5,20 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?=$text?></title>
 	<style type="text/css">
-	body { background-color: #999; margin: 2em; font-family: Arial, sans-serif; }
-	#wrap { border: 10px solid #ccc; background-color: #eee; width: 40em; }
-	#wrap { text-align: center; margin: 0 auto; }
-	h1 { font-size: 1.3em; }
+	body { background-color: #999; margin: 2em; font-family: Verdana, Arial, sans-serif; }
+	a { color: #000; }
+	#wrap { border: 10px solid #ccc; background: #eee url(<?=METABBS_BASE_PATH?>themes/default/error.png) no-repeat 1em 1em; width: 30em; }
+	#wrap { margin: 0 auto; padding: 1em 0 1em 6em; }
+	h1 { font-size: 1.3em; margin: 0.25em 0 0.5em 0; }
 	p { font-size: 0.8em; }
+	#metabbs { font-size: 0.6em; color: #666; margin-top: 3em; }
 	</style>
 </head>
 <body>
 <div id="wrap">
-<p><img src="<?=METABBS_BASE_PATH?>themes/default/error.png" alt="error" /></p>
 <h1><?=$text?></h1>
 <p><?=$description?></p>
+<p id="metabbs">Powered by <a href="http://metabbs.org/">MetaBBS</a> (<?=METABBS_VERSION?>)</p>
 </div>
 </body>
 </html>
