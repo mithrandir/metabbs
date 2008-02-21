1fd38264e82c2a7ca0741e23a66dcec37d7c9ef6
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-21 00:43:53 +0000

    #176 작업중

--- metabbs/app/views/account/login.php
+++ metabbs/app/views/account/login.php
@@ -1,12 +1,3 @@
-<h2>로그인</h2>
-<form method="post" action="">
-<dl>
-	<dt>아이디</dt>
-	<dd><input type="text" name="user" /></dd>
-
-	<dt>암호</dt>
-	<dd><input type="password" name="password" /></dd>
-</dl>
-<p><input type="checkbox" name="autologin" id="autologin" value="1" /> <label for="autologin">자동 로그인</label></p>
-<p><input type="submit" value="로그인" /> <a href="<?=url_with_referer_for('account', 'signup')?>">회원가입</a></p>
-</form>
+<?php
+include 'themes/default/login.php';
+?>
