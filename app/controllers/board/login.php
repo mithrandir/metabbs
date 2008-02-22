cb1e1105c4c881c8b6704e71423e00ee8322fd6d
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-22 08:22:29 +0000

    board/login 컨트롤러 없애기

--- app/controllers/board/login.php
+++ app/controllers/board/login.php
@@ -1,10 +1,5 @@
 <?php
-if (is_post()) {
-	$controller = 'account';
-	$action = 'login';
-	include 'app/controllers/account/login.php';
-} else {
-	$template = get_template($board, 'login');
-	$template->set('board', $board);
-}
+$controller = 'account';
+$action = 'login';
+include 'app/controllers/account/login.php';
 ?>
