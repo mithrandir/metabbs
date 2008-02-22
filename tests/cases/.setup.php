e1311278d61fa08cce4f1b903c5798968234c5a6
lagendia <lagendia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-22 06:31:02 +0000

    config.php 의존성을 원래대로  되돌림.

--- tests/cases/.setup.php
+++ tests/cases/.setup.php
@@ -5,11 +5,10 @@ require_once "../lib/model.php";
 require_once "../lib/query.php";
 require_once "../lib/backends/mysql/backend.php";
 require_once "../lib/backends/mysql/installer.php";
-require_once "config.php";
 
-//$username = "root";
-//$password = "";
-//$testdb="metabbs_test";
+$username = "root";
+$password = "";
+$testdb="metabbs_test";
 
 $__db = new MySQLConnection;
 $__db->connect("localhost", $username, $password);
--- tests/cases/mysql_test.php
+++ tests/cases/mysql_test.php
@@ -1,29 +1,20 @@
 <?php
 require_once '../lib/backends/mysql/backend.php';
-require_once 'config.php';
 
 class MySQLTest extends UnitTestCase {
 	function setUp() {
 		$this->conn = new MySQLConnection;
 	}
 	function testOpen() {
-		//$username = "root";
-		//$password = "";
-		//$dbname="metabbs_test";
+		$username = "root";
+		$password = "";
+		$testdb="metabbs_test";
 		$this->conn->open(array(
 			"host" => "localhost",
 			"user" => $username,
 			"password" => $password,
 			"dbname" => $testdb
 		));
-	function testTo_spec() {
-		var $name = "testname";
-		$test_int = to_spec($name, "Integer", null); 
-		$this->assertEqual($test_int, "`testname` integer(10) NOT NULL DEFAULT '0'");
-		$test_str = to_spec($name, "String", 1);
-		$this->assertEqual($name, "`testname` varchar(1) NOT NULL DEFAULT ''");
-		$test_bool = to_spec($name, "Boolean", null);
-		$this->assertEqual($name, "`testname` bool NOT NULL");
 	}
 }
 ?>
