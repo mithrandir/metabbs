e832bd81a12c40ae9c1565a8a2805b86b9dcfe7e
lagendia <lagendia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-22 04:39:18 +0000

    MySQL 백엔드 유닛테스트 추가

--- woc/tests/cases/mysql_test.php
+++ woc/tests/cases/mysql_test.php
@@ -16,6 +16,14 @@ class MySQLTest extends UnitTestCase {
 			"password" => $password,
 			"dbname" => $testdb
 		));
+	function testTo_spec() {
+		var $name = "testname";
+		$test_int = to_spec($name, "Integer", null); 
+		$this->assertEqual($test_int, "`testname` integer(10) NOT NULL DEFAULT '0'");
+		$test_str = to_spec($name, "String", 1);
+		$this->assertEqual($name, "`testname` varchar(1) NOT NULL DEFAULT ''");
+		$test_bool = to_spec($name, "Boolean", null);
+		$this->assertEqual($name, "`testname` bool NOT NULL");
 	}
 }
 ?>
