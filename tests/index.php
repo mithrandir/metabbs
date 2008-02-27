d7f407cc05a9ee9ee75efaf7447783312251ee48
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-27 12:34:11 +0000

    sqlite_index.php와 index.php 합침 (http://url/to/metabbs/tests/?backend=sqlite로 접속하면 됩니다)

--- tests/index.php
+++ tests/index.php
@@ -8,11 +8,14 @@ while ($f = readdir($d)) {
 		$cases[] = $matches[1];
 }
 
+$backend = isset($_GET['backend']) ? $_GET['backend'] : 'mysql';
+
 function print_case_list($cases) {
+	global $backend;
 	echo "<p>";
-	echo "<a href=\"?test=all\">All</a>";
+	echo "<a href=\"?backend=$backend&amp;test=all\">All</a>";
 	foreach ($cases as $case) {
-		echo " | <a href=\"?test=$case\">$case</a>";
+		echo " | <a href=\"?backend=$backend&amp;test=$case\">$case</a>";
 	}
 	if (file_exists('coverage'))
 		echo " || <a href=\"coverage/\">Coverage Report</a>";
@@ -34,7 +37,10 @@ if (!isset($_GET['test'])) {
 	echo "<h2>Select the test case</h2>";
 	print_case_list($cases);
 } else {
-	include 'cases/.setup.php';
+	if ($backend == 'sqlite')
+		include 'cases/.sqlite_setup.php';
+	else
+		include 'cases/.setup.php';
 
 	$test = &new TestSuite('MetaBBS Tests');
 	if (in_array($_GET['test'], $cases)) {
@@ -44,6 +50,9 @@ if (!isset($_GET['test'])) {
 	}
 	$test->run(new ExtendedHtmlReporter($cases));
 
-	include 'cases/.teardown.php';
+	if ($backend == 'sqlite')
+		include 'cases/.sqlite_teardown.php';
+	else
+		include 'cases/.teardown.php';
 }
 ?>
