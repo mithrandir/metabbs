9df6c9798f1d188e23ecf9bc0b40e8a585b26b61
lagendia <lagendia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-02-29 04:01:13 +0000

    어제 미팅의 결과물입니다. base.php 의 에러메시지가 더 친절하게 나옵니다. 테스트 통과를 위해 빈 클래스와 함수들이 추가되었습니다.

--- woc/lib/backends/sqlite/backend.php
+++ woc/lib/backends/sqlite/backend.php
@@ -1,17 +1,38 @@
 <?php
 require_once dirname(__FILE__).'/../base.php';
 
-//Test 통과를 위한 dummy code
-class stringColumn {
+class StringColumn {
 	function to_spec() {
 	}
 }
-//dummy code 끝
+
+class IntegerColumn {
+	function to_spec() {
+		return '';
+	}
+}
+
+class BooleanColumn {
+	function to_spec() {
+		return '';
+	}
+}
+
+class UshortColumn {
+	function to_spec() {
+		return '';
+	}
+}
 
 class SQLiteConnection extends BaseConnection {
 	var $conn;
 	function open($info) {
 		$this->conn = sqlite_open($info["dbname"], 0666, $error) or trigger_error($error, E_USER_ERROR);
+	}
+	function add_table() {
+
+	}
+	function query() {
 	}
 	function close($conn) {
 		sqlite_close($conn);
