db08621174e5cfc96b28ded456b484f98a46716a
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-03-02 13:36:03 +0000

    XSS

--- themes/default/user-info.php
+++ themes/default/user-info.php
@@ -1,12 +1,12 @@
 <h1>User Information</h1>
 <div id="profile">
-<p><span class="name"><?=$user->name?></span> (<?=$user->user?>)</p>
+<p><span class="name"><?=$user->name?></span> (<?=htmlspecialchars($user->user)?>)</p>
 <p>
 <? if ($user->email) { ?>
-E-mail: <a href="mailto:<?=$user->email?>"><?=str_replace("@", " at ", $user->email)?></a><br />
+E-mail: <a href="mailto:<?=htmlspecialchars($user->email)?>"><?=str_replace("@", " at ", htmlspecialchars($user->email))?></a><br />
 <? } ?>
 <? if ($user->url) { ?>
-Homepage: <?=link_text($user->get_url())?><br />
+Homepage: <?=link_text(htmlspecialchars($user->get_url()))?><br />
 <? } ?>
 </p>
 <p><?=$user->get_post_count()?> posts, <?=$user->get_comment_count()?> comments</p>
