9cea662ae719f72396f0512eba080a5042ec03cd
dahlia <dahlia@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-04-28 14:38:47 +0000

    엄격한 XHTML 적용을 도와주는 TidyIt (tidy 확장 이용) 플러그인

---
+++ plugins/TidyIt.php
@@ -0,0 +1,44 @@
+<?php
+
+if (!extension_loaded('tidy')) return;
+
+class TidyIt extends Plugin {
+	var $plugin_name = 'Tidy It!';
+	var $description = '사용자가 입력한 HTML에 대해 엄격하게 검사합니다.';
+	var $tidy;
+
+	var $config = array(
+		'output-xhtml' => true,
+		'wrap' => 0,
+		'drop-empty-paras' => true,
+		'drop-font-tags' => true,
+		'drop-proprietary-attributes' => true,
+		'logical-emphasis' => true,
+		'lower-literals' => true
+	);
+
+	function on_init() {
+		$this->tidy = new tidy;
+
+		add_filter('PostList', array(&$this, 'format'), 2000);
+		add_filter('PostView', array(&$this, 'format'), 2000);
+		add_filter('PostViewRSS', array(&$this, 'format'), 2000);
+		add_filter('PostViewComment', array(&$this, 'format'), 2000);
+	}
+
+	function format(&$model) {
+		if (!$model->body) return;
+
+		$this->tidy->parseString($model->body, $this->config, 'utf8');
+		$this->tidy->cleanRepair();
+
+		$model->body = ereg_replace(
+			'(^[[:space:]]*<body>|</body>[[:space:]]*$)', '',
+			$this->tidy->body()
+		);
+	}
+}
+
+register_plugin('TidyIt');
+
+?>
