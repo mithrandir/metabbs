7310b8e5ce81d7d7bc71209522de479054b0f9ec
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-06-21 14:19:38 +0000

    r1204 계속

--- metabbs/app/controllers/board/atom.php
+++ metabbs/app/controllers/board/atom.php
@@ -1,8 +1,4 @@
 <?php
 require_once 'lib/feed.php';
-
 permission_required('list', $board);
-
-$title = $board->get_title();
-feed_render($title, full_url_for($board), "The latest posts from $title",
-		$board->get_feed_posts($board->posts_per_page), 'atom');
+render_board_feed($board, 'atom');
