8dae227f1e2139aa7ccbf185c39b4378ea27afe0
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-06-21 14:15:04 +0000

    피드 기능 리팩토링

--- app/controllers/board/rss.php
+++ app/controllers/board/rss.php
@@ -1,30 +1,8 @@
 <?php
 require_once 'lib/feed.php';
-$posts = feed_render_header($board, 'rss');
-?>
-<rss version="2.0">
-	<channel>
-		<title><?=htmlspecialchars($board->get_title())?></title>
-		<link><?=full_url_for($board)?></link>
-		<description>The latest posts from <?=htmlspecialchars($board->get_title())?></description>
-<? if (!empty($posts)) { ?>
-		<pubDate><?=meta_format_date_RFC822($posts[0]->created_at)?></pubDate>
-<? } ?>
-<? foreach ($posts as $post) { ?>
-		<item>
-			<title><![CDATA[<?=$post->title?>]]></title>
-			<link><?=full_url_for($post)?></link>
-			<description><? if (!$post->secret) { ?><![CDATA[
-	<? foreach ($post->get_attachments() as $attachment) { ?>
-	<? if ($attachment->is_image()) { ?><img src="<?=full_url_for($attachment)?>" alt="<?=$attachment->filename?>" /><? } } ?>
-<?=format($post->body)?>]]><? } ?></description>
-			<author><![CDATA[<?=$post->name?>]]></author>
-			<pubDate><?=meta_format_date_RFC822($post->created_at)?></pubDate>
-<? if ($board->use_category && $category = $post->get_category()) { ?>
-			<category><?=htmlspecialchars($category->name)?></category>
-<? } ?>
-		</item>
-<? } ?>
-	</channel>
-</rss>
-<? exit; ?>
+
+permission_required('list', $board);
+
+$title = $board->get_title();
+feed_render($title, full_url_for($board), "The latest posts from $title",
+		$board->get_feed_posts($board->posts_per_page), 'rss');
--- lib/feed.php
+++ lib/feed.php
@@ -1,17 +1,8 @@
 <?php
-permission_required('list', $board);
-
-/**
- * 피드의 해더를 꾸며준다.
- * @param $board 게시판 명
- * @param $format 가져올 포맷의 이름
- * @return 피드를 리턴한다.
- */
-function feed_render_header($board, $format) {
+function feed_render($title, $url, $description, $posts, $format) {
+	apply_filters_array('PostViewRSS', $posts);
 	header("Content-Type: text/xml; charset=UTF-8");
 	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
-	$posts = $board->get_feed_posts($board->posts_per_page);
-	apply_filters_array('PostViewRSS', $posts);
-	return $posts;
+	include "app/views/feed/$format.php";
+	exit;
 }
-?>
