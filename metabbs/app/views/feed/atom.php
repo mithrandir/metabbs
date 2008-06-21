5e709d9933960b7ed15ae72ff5924b33940294c8
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-06-21 14:15:04 +0000

    피드 기능 리팩토링

---
+++ metabbs/app/views/feed/atom.php
@@ -0,0 +1,25 @@
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<title><?=htmlspecialchars($title)?></title>
+	<id><?=$url?></id>
+<? if (!empty($posts)) { ?>
+	<updated><?=meta_format_date_RFC822($posts[0]->created_at)?></updated>
+<? } ?>
+	<subtitle><?=htmlspecialchars($description)?></subtitle>
+<? foreach ($posts as $post) { ?>
+	<entry>
+		<title><![CDATA[<?=$post->title?>]]></title>
+		<id><?=full_url_for($post)?></id>
+<? if (!$post->secret) { ?>
+		<content type="html"><? if (!$post->secret) { ?><![CDATA[
+	<? foreach ($post->get_attachments() as $attachment) { ?>
+	<? if ($attachment->is_image()) { ?><img src="<?=full_url_for($attachment)?>" alt="<?=$attachment->filename?>" /><? } } ?>
+<?=format($post->body)?>]]><? } ?></content>
+<? } ?>
+		<author><name><![CDATA[<?=$post->name?>]]></name></author>
+		<updated><?=meta_format_date_RFC822($post->created_at)?></updated>
+<? if ($board->use_category && $category = $post->get_category()) { ?>
+		<category term="<?=htmlspecialchars($category->name)?>" />
+<? } ?>
+	</entry>
+<? } ?>
+</feed>
