aff6f8f45cb99dc8b78fa9b9af84c9dc5d18cd25
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-07-29 15:25:24 +0000

    #204 썸네일 생성 권한 수정

--- app/controllers/attachment/index.php
+++ app/controllers/attachment/index.php
@@ -12,6 +12,7 @@ if (isset($_GET['thumb'])) {
 	$ext = get_image_extension($orig_path);
 	$thumb_path = 'data/thumb/'.$attachment->id.'-small.'.$ext;
 	if (create_thumbnail($orig_path, $thumb_path)) {
+		chmod($thumb_path, 0606);
 		redirect_to(METABBS_BASE_PATH.$thumb_path);
 	}
 }
