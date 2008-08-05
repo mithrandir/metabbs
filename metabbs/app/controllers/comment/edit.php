85ddf5db3704ed4f2e158314000bb94dc54bcb93
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-08-05 13:51:42 +0000

    #171: 고칠 때도

--- metabbs/app/controllers/comment/edit.php
+++ metabbs/app/controllers/comment/edit.php
@@ -5,7 +5,18 @@ $post = $comment->get_post();
 if (is_post() && (!$account->is_guest() || $comment->password == md5($_POST['password']))) {
 	$comment->body = $_POST['body'];
 	$comment->update();
-	redirect_to(url_for($post) . '#comment_' . $comment->id);
+
+	if (!is_xhr())
+		redirect_to(url_for($post) . '#comment_' . $comment->id);
+	else {
+		$board = $comment->get_board();
+		apply_filters('PostViewComment', $comment);
+		$template = get_template($board, '_comment');
+		$template->set('board', $board);
+		$template->set('comment', $comment);
+		$template->render_partial();
+		exit;
+	}
 } else {
 	$template = get_template($board, 'edit_comment');
 	$template->set('board', $board);
