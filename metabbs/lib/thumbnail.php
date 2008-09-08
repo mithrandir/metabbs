662f37f67d47427e35cc79abeea5b8a34fca6c9b
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-09-08 09:03:23 +0000

    #220 기능추가

--- metabbs/lib/thumbnail.php
+++ metabbs/lib/thumbnail.php
@@ -16,12 +16,76 @@ function get_image_extension($path) {
 		return null;
 }
 
-function create_thumbnail($path, $dest) {
+function create_thumbnail($path, $dest, $kind = 0, $size = 130) {
 	if (!file_exists($dest)) {
 		ini_set('memory_limit', '-1');
 		list($width, $height, $type) = getimagesize($path);
-		if ($height <= 130) return false;
-		$thumb = imagecreatetruecolor($width * (130 / $height), 130);
+		switch($kind) {
+			// 가로폭이 size로 고정 축소
+			case 0:
+				if ($width <= $size || $height <= $size) return false;
+				$src_x = 0;
+				$src_y = 0;
+				$src_w = $width;
+				$src_h = $height;
+				$dst_x = 0;
+				$dst_y = 0;
+				$dst_w = $width * ($size / $height);
+				$dst_h = $size;
+				break;
+			// 큰축을 size로 고정 축소
+			case 1:
+				if ($width <= $size || $height <= $size) return false;
+				$src_x = 0;
+				$src_y = 0;
+				$src_w = $width;
+				$src_h = $height;
+				$dst_x = 0;
+				$dst_y = 0;
+				if($width > $height) {
+					$dst_w = $width * ($size / $height);
+					$dst_h = $size;
+				} else {
+					$dst_w = $size;
+					$dst_h = $height * ($size / $width);
+				}
+				break;
+			// 작은축을 size로 고정 축소
+			case 2:
+				if ($width <= $size || $height <= $size) return false;
+				$src_x = 0;
+				$src_y = 0;
+				$src_w = $width;
+				$src_h = $height;
+				$dst_x = 0;
+				$dst_y = 0;
+				if($width > $height) {
+					$dst_w = $width / $height * $size;
+					$dst_h = $size;
+				} else {
+					$dst_w = $size;
+					$dst_h = $height / $width * $size;
+				}
+				break;
+			// size의 정사각형으로 잘라내기 축소
+			case 3:
+				if ($width <= $size || $height <= $size) return false;
+				$src_x = 0;
+				$src_y = 0;
+				if($width > $height) {
+					$src_x = ($width - $height) / 2;
+					$src_w = $src_h = $height;
+				} else {
+					$src_y = ($height - $width) / 2;
+					$src_w = $src_h = $width;
+				}
+				$dst_x = 0;
+				$dst_y = 0;
+				$dst_w = $size;
+				$dst_h = $size;
+				break;
+		}
+		$thumb = imagecreatetruecolor($dst_w, $dst_h);
 		if ($type == 1) {
 			$source = imagecreatefromgif($path);
 			$func = 'imagegif';
@@ -34,7 +98,7 @@ function create_thumbnail($path, $dest) {
 		} else {
 			return;
 		}
-		imagecopyresampled($thumb, $source, 0, 0, 0, 0, imagesx($thumb), imagesy($thumb), $width, $height);
+		imagecopyresampled($thumb, $source, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
 		return $func($thumb, $dest);
 	}
 	return true;
