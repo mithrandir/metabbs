a3a7bcaf42d6f45ec415bd0116f960d398ff03fe
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-09-25 03:21:39 +0000

    #220 기능 추가

--- lib/thumbnail.php
+++ lib/thumbnail.php
@@ -16,26 +16,26 @@ function get_image_extension($path) {
 		return null;
 }
 
-function create_thumbnail($path, $dest, $kind = 0, $size = 130) {
+function create_thumbnail($path, $dest, $kind = 0, $options = array('size'=>130)) {
 	if (!file_exists($dest)) {
 		ini_set('memory_limit', '-1');
 		list($width, $height, $type) = getimagesize($path);
 		switch($kind) {
 			// 가로폭이 size로 고정 축소
 			case 0:
-				if ($width <= $size || $height <= $size) return false;
+				if ($width <= $options['size'] || $height <= $options['size']) return false;
 				$src_x = 0;
 				$src_y = 0;
 				$src_w = $width;
 				$src_h = $height;
 				$dst_x = 0;
 				$dst_y = 0;
-				$dst_w = $width / $height * $size;
-				$dst_h = $size;
+				$dst_w = $width / $height * $options['size'];
+				$dst_h = $options['size'];
 				break;
 			// 작은축을 size로 고정 축소
 			case 1:
-				if ($width <= $size || $height <= $size) return false;
+				if ($width <= $options['size'] || $height <= $options['size']) return false;
 				$src_x = 0;
 				$src_y = 0;
 				$src_w = $width;
@@ -43,16 +43,16 @@ function create_thumbnail($path, $dest, $kind = 0, $size = 130) {
 				$dst_x = 0;
 				$dst_y = 0;
 				if($width > $height) {
-					$dst_w = $width / $height * $size;
-					$dst_h = $size;
+					$dst_w = $width / $height * $options['size'];
+					$dst_h = $options['size'];
 				} else {
-					$dst_w = $size;
-					$dst_h = $height / $width * $size;
+					$dst_w = $options['size'];
+					$dst_h = $height / $width * $options['size'];
 				}
 				break;
 			// 큰축을 size로 고정 축소
 			case 2:
-				if ($width <= $size || $height <= $size) return false;
+				if ($width <= $options['size'] || $height <= $options['size']) return false;
 				$src_x = 0;
 				$src_y = 0;
 				$src_w = $width;
@@ -60,16 +60,16 @@ function create_thumbnail($path, $dest, $kind = 0, $size = 130) {
 				$dst_x = 0;
 				$dst_y = 0;
 				if($width > $height) {
-					$dst_w = $size;
-					$dst_h = $height / $width * $size;
+					$dst_w = $options['size'];
+					$dst_h = $height / $width * $options['size'];
 				} else {
-					$dst_w = $width / $height * $size;
-					$dst_h = $size;
+					$dst_w = $width / $height * $options['size'];
+					$dst_h = $options['size'];
 				}
 				break;
 			// size의 정사각형으로 잘라내기 축소
 			case 3:
-				if ($width <= $size || $height <= $size) return false;
+				if ($width <= $options['size'] || $height <= $options['size']) return false;
 				$src_x = 0;
 				$src_y = 0;
 				if($width > $height) {
@@ -81,9 +81,30 @@ function create_thumbnail($path, $dest, $kind = 0, $size = 130) {
 				}
 				$dst_x = 0;
 				$dst_y = 0;
-				$dst_w = $size;
-				$dst_h = $size;
+				$dst_w = $options['size'];
+				$dst_h = $options['size'];
 				break;
+			// width, height 로 zoom & crop
+			case 4:
+				if ($width <= $options['width'] || $height <= $options['height']) return false;
+				$src_x = 0;
+				$src_y = 0;
+				$src_w = $width;
+				$src_h = $height;
+				$dst_x = 0;
+				$dst_y = 0;
+				$dst_w = $options['width'];
+				$dst_h = $options['height'];
+				$cmp_x = $width / $options['width'];
+				$cmp_y = $height / $options['height'];
+
+				if($cmp_x > $cmp_y) {
+					$src_w = round( ( $width / $cmp_x * $cmp_y ) );
+					$src_x = round( ( $width - ( $width / $cmp_x * $cmp_y ) ) / 2 );
+				} elseif ( $cmp_y > $cmp_x ) {
+					$src_h = round( ( $height / $cmp_y * $cmp_x ) );
+					$src_y = round( ( $height - ( $height / $cmp_y * $cmp_x ) ) / 2 );
+				}
 		}
 		$thumb = imagecreatetruecolor($dst_w, $dst_h);
 		if ($type == 1) {
