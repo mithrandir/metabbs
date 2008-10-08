1f2e8bcabefba50118d1559a2e7c4d89ae0f8f72
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-10-08 09:49:50 +0000

    사용자 관련 이벤트 시점 추가 및 모델기능 추가

--- app/controllers/account/login.php
+++ app/controllers/account/login.php
@@ -1,6 +1,7 @@
 <?php
 if (is_post()) {
 	$account = UserManager::login($_POST['user'], $_POST['password'], isset($_POST['autologin']));
+	apply_filters('BeforeLogin', $account, $_POST);
 	if (!$account) {
 		$account = new Guest;
 		$error->add('Login failed');
