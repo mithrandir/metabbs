39e0e6fd80027f184c2e868abe2c83d930ad785a
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2008-11-17 05:43:38 +0000

    새 주소 체계 적용

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -3,9 +3,9 @@ if ($action != 'edit') {
 	if (!isset($id)) {
 		print_notice(i('No user id'), i('Please append the user id.'));
 	}
-	$user = User::find($id);
+	$user = User::find_by_user($id);
 	if (!$user->exists()) {
-		print_notice(i('User not found'), i("User #%d doesn't exist.", $id));
+		print_notice(i('User not found'), i("User '%s' doesn't exist.", htmlspecialchars($id)));
 	}
 }
 $title = htmlspecialchars($user->name);
