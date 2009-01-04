eeeba020c8fb0469777a9fb15be2a78b0c241dee
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-01-04 09:45:15 +0000

    갤러리 스킨 댓글 버그

--- metabbs/skins/gallery-default/skin.php
+++ metabbs/skins/gallery-default/skin.php
@@ -3,7 +3,8 @@ $template_engine = 'standard';
 $options = array(
 	'get_body_in_the_list' => false,
 	'preload_attachments' => true,
-	'exclude_notice' => true
+	'exclude_notice' => true,
+	'build_comment_tree' => false
 );
 
 $GLOBALS['layout']->add_stylesheet(METABBS_BASE_PATH.$this->get_path().'/lightbox/css/lightbox.css');
