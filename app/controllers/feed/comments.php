7bcf3dc2d66c4677d28d82e3fa66a7e6584bdc8a
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-01-24 09:09:31 +0000

    * [1381], [1382] 실수 바로잡음 * 이모티콘 플러그인: 댓글 피드에도 포매팅 적용

--- app/controllers/feed/comments.php
+++ app/controllers/feed/comments.php
@@ -4,7 +4,7 @@ require_once 'core/feed.php';
 $title = 'Full site feed (comments)';
 $url = url_for('feed', 'comments');
 $comments = Site::get_latest_comments(20);
-apply_filters('CommentViewRSS', $comments);
+apply_filters_array('CommentViewFeed', $comments);
 
 feed_header();
 include 'app/views/feed/comments.php';
