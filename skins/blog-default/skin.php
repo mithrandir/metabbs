240670bfd6727f1df427c3c216543ff942a41ca5
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-01-25 10:36:16 +0000

    블로그 스킨 목록에서 첨부 파일 출력

--- skins/blog-default/skin.php
+++ skins/blog-default/skin.php
@@ -1,6 +1,6 @@
 <?php
 $template_engine = 'standard';
-$options = array('get_body_in_the_list' => true, 'build_comment_tree' => false);
+$options = array('get_body_in_the_list' => true, 'build_comment_tree' => false, 'preload_attachments' => true);
 
 if (!function_exists('blog_url')) {
 	function blog_url($board) {
