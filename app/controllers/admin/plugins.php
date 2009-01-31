fff116b21750e27f4c8d77be64df006388bac7cc
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-01-31 01:25:25 +0000

    플러그인 이름을 가나다순 정렬

--- app/controllers/admin/plugins.php
+++ app/controllers/admin/plugins.php
@@ -1,3 +1,10 @@
 <?php
 $plugins = get_plugins();
+
+function compare_plugin_names($a, $b) {
+	if ($a->plugin_name == $b->plugin_name)
+		return 0;
+	return ($a->plugin_name < $b->plugin_name) ? -1 : 1;
+}
+usort($plugins, 'compare_plugin_names');
 ?>
