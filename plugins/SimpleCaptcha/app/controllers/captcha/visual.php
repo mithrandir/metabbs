c5180e953c34b46271319f15eae93a26c235a87c
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-02-01 07:08:01 +0000

    SimpleCaptcha 플러그인 위치 변경

--- plugins/SimpleCaptcha/app/controllers/captcha/visual.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fonts = SimpleCaptcha::get_fonts();
-$oVisualCaptcha = new PhpCaptcha($fonts, 120, 18);
-$oVisualCaptcha->SetMinFontSize(12);
-$oVisualCaptcha->SetMaxFontSize(15);
-$oVisualCaptcha->Create();
-?>
