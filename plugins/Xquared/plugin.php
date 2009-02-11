6923b0989ac8ca57ab8a3d58e2b82826a9efeec7
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-02-11 16:59:25 +0000

    Xquared 판올림(0.7, 20080402)

--- plugins/Xquared/plugin.php
+++ plugins/Xquared/plugin.php
@@ -1,42 +1,43 @@
 <?php
 class Xquared extends Plugin {
-	var $plugin_name = 'Xquared 위지윔 편집기';
+	var $plugin_name = 'Xquared 위지윔 편집기(0.7)';
 	var $description = '글 입력창을 Xquared 위지윔 편집기로 바꿉니다.';
 
 	function on_init() {
 		global $controller, $action, $layout;
 
 		ini_set('include_path', dirname(__FILE__).PATH_SEPARATOR.ini_get('include_path'));
-		if(!class_exists('HTML_Safe'))
-			include_once 'HTML_Safe.php';
+//		if(!class_exists('HTML_Safe'))
+//			include_once 'HTML_Safe.php';
 
 		add_filter('PostList', array(&$this, 'format'), 500);
 		add_filter('PostView', array(&$this, 'format'), 500);
 		add_filter('PostViewRSS', array(&$this, 'format'), 500);
 
-		$plugin_uri = METABBS_BASE_PATH.'plugins/Xquared';
-		$layout->add_stylesheet("$plugin_uri/css/xq_custom.css");
-
-		if($controller == 'board' and $action == 'post' or $controller == 'post' and $action == 'edit') {
-
+		if($controller == 'board' and $action == 'post' or $controller == 'post' and $action == 'edit')
 			$this->enable_editor();
-		}
 	}
 
 	function enable_editor() {
 		global $layout;
 
 		$plugin_uri = METABBS_BASE_PATH.'plugins/Xquared';
-		$layout->add_stylesheet("$plugin_uri/css/xq_contents.css");
-		$layout->add_javascript("$plugin_uri/js/xquared-min.js");
-		$layout->add_stylesheet("$plugin_uri/css/xq_ui.css");
+		$layout->add_stylesheet("$plugin_uri/xquared/stylesheets/xq_ui.css");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/XQuared.js?load_others=1");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/SpringnotePlugin.js");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/EditorResizePlugin.js");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/MacroPlugin.js");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/FlashMovieMacroPlugin.js");
+		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/IFrameMacroPlugin.js");
+//		$layout->add_javascript("$plugin_uri/xquared/javascripts/plugin/JavascriptMacroPlugin.js");
+
 		$layout->add_javascript("$plugin_uri/plugin.js");
 		$layout->header = "
-			<script type=\"text/javascript\">
-			// <![CDATA[
-			var XquaredPluginUri = '$plugin_uri';
-			// ]]>
-			</script>
+<script type=\"text/javascript\">
+// <![CDATA[
+var XquaredPluginUri = '$plugin_uri';
+// ]]>
+</script>
 		".$layout->header;
 	}
 
@@ -47,8 +48,9 @@ class Xquared extends Plugin {
 		if($post->get_attribute('format') != 'xquared-html')
 			$post->body = format_plain($post->body);
 		else {
-			$safe = new HTML_Safe;
-			$post->body = '<div class="xed">'.$safe->parse($post->body).'</div>';
+//			$safe = new HTML_Safe;
+//			$post->body = '<div class="xed">'.$safe->parse($post->body).'</div>';
+			$post->body = '<div class="xed">'.$post->body.'</div>';
 		}
 	}
 }
