69fa62469e4da87cfcc9fd050e46cf9554c0de9a
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-02-15 12:15:51 +0000

    #195 처리

--- metabbs/app/controllers/board/comments-feed.php
+++ metabbs/app/controllers/board/comments-feed.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'core/feed.php';
 
-$title = 'Full site feed (comments)';
-$url = full_url_for('feed', 'comments');
-$comments = Site::get_latest_comments(20);
+$title = $board->get_title();
+$url = full_url_for($board, 'comments-feed');
+$comments = $board->get_recent_comments(20);
 apply_filters_array('CommentViewFeed', $comments);
 
 feed_header();
