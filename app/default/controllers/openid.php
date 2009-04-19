012dae13ea80f4dc6b27764b531e1d54a6454e10
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-04-19 14:15:14 +0000

    openid 플러그인 core로 합류

---
+++ app/default/controllers/openid.php
@@ -0,0 +1,21 @@
+<?php
+if (!file_exists('/dev/urandom')) {
+	define('Auth_OpenID_RAND_SOURCE', NULL);
+}
+define('Auth_OpenID_NO_MATH_SUPPORT', 1);
+
+ini_set("include_path", 'core/external/php-openid' . PATH_SEPARATOR . ini_get("include_path"));
+
+require_once 'Auth/OpenID.php';
+require_once 'Auth/OpenID/Consumer.php';
+require_once 'Auth/OpenID/FileStore.php';
+
+$store_path = METABBS_DIR . '/data/openid';
+
+if (!file_exists($store_path) && !mkdir($store_path)) {
+	print_notice('Cannot create OpenID store directory.', 'Please check the permission of ' . METABBS_DIR . '/data.');
+}
+
+$store = new Auth_OpenID_FileStore($store_path);
+$consumer = new Auth_OpenID_Consumer($store);
+?>
--- themes/default/login.php
+++ themes/default/login.php
@@ -3,7 +3,7 @@
 <?=flash_message_box()?>
 <?=error_message_box($error_messages)?>
 
-<form method="post" action="" id="login-form">
+<form method="post" action="<?=url_with_referer_for('account', 'login') ?>" id="login-form">
 <p><label for="user" class="field"><?=i('User ID')?></label> <input type="text" name="user" id="user" /></p>
 <p><label for="password" class="field"><?=i('Password')?></label> <input type="password" name="password" id="password" /></p>
 <p><input type="checkbox" name="autologin" id="autologin" value="1" /> <label for="autologin"><?=i('Auto Login')?></label></p>
@@ -12,4 +12,10 @@
 <? if($config->get('use_forget_password', false)): ?> <a href="<?=url_with_referer_for('account', 'forget-password')?>"><?=i('Forget Password')?></a><? endif; ?>
 <? if (!is_xhr() && isset($_GET['url']) && !empty($_GET['url'])): ?> <a href="<?=$_GET['url']?>" class="button dialog-close"><?=i('Cancel')?></a><? endif; ?>
 </p>
+</form>
+
+<form method="post" action="<?=url_with_referer_for('openid', 'login')?>" id="openid-form">
+<h2><?= i('OpenID Login')?></h2>
+<p><input type="text" name="openid_identifier" style="background: #fff url(<?=METABBS_BASE_PATH?>media/login-openid.gif) no-repeat 0 50%; padding-left: 18px;" /> <input type="submit" value="<?=i('Login')?>" /></p>
+<p><input type="checkbox" name="autologin" value="1" id="openid_autologin" /> <label for="openid_autologin"><?=i('Auto Login')?></label></p>
 </form>
