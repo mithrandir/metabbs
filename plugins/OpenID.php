323db75aae027f12ca934c604ba1e080d2dadb80
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-04-21 23:17:33 +0000

    [1457] 비활성화 하도록 함

--- plugins/OpenID.php
+++ plugins/OpenID.php
@@ -1,7 +1,9 @@
 <?php
 global $config;
 
-if (Plugin::is_enabled('OpenID') && !using_openid()) {
+$openid_plugin = find_by('plugin', 'name', 'OpenID');
+if ($openid_plugin->enabled && !using_openid()) {
 	$config->set('authentication', AUTH_DEFAULT | AUTH_OPENID);
 	$config->write_to_file();
+	$openid_plugin->disable();
 }
