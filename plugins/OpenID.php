477f1eadac205e0e1d4e12e9e953d59193dacb40
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-04-21 23:16:12 +0000

    OpenID 모델 클래스와 충돌하지 않도록 조정

--- plugins/OpenID.php
+++ plugins/OpenID.php
@@ -1,11 +1,7 @@
 <?php
 global $config;
 
-class OpenID extends Plugin { } // dummy
-
-$openid_plugin = Plugin::find_by_name('OpenID');
-if ($openid_plugin->enabled) {
-	$openid_plugin->disable();
+if (Plugin::is_enabled('OpenID') && !using_openid()) {
 	$config->set('authentication', AUTH_DEFAULT | AUTH_OPENID);
 	$config->write_to_file();
 }
