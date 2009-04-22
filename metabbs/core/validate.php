01822442da3bab26c30011d8d172f304fe2cb1a2
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-04-22 03:37:52 +0000

    account/edit도 변경

--- metabbs/core/validate.php
+++ metabbs/core/validate.php
@@ -8,6 +8,7 @@ class Validate {
 		if (strlen($value) > 255)
 			return false;
 		$parts = explode('@', $value, 2);
+
 		return ((count($parts) == 2) && preg_match('@[\\w!#\-\'*+/=?^`{-~-]+(\\.[\\w!#-\'*+/=?^`{-~-]+)*@', $parts[0]) && Validate::domain($parts[1]));
 	}
 	
