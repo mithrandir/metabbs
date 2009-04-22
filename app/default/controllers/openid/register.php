005e3a669963df1a1e72ad225d9183eac9046f4b
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-04-22 15:08:12 +0000

    #233 openid관련 redirect 조절

--- app/default/controllers/openid/register.php
+++ app/default/controllers/openid/register.php
@@ -6,9 +6,11 @@ if (isset($_REQUEST['openid_identifier']) and !empty($_REQUEST['openid_identifie
 	}
 	$auth_request = $consumer->begin($openid);
 	if ($auth_request) {
-		redirect_to($auth_request->redirectURL(METABBS_HOST_URL . METABBS_BASE_PATH, full_url_with_referer_for('openid', 'register_complete')));
+		redirect_to($auth_request->redirectURL(METABBS_HOST_URL . METABBS_BASE_PATH, full_url_for('openid', 'register_complete', array('url'=>url_with_referer_for('account','edit')))));
 	} else {
 		Flash::set('OpenID\'s not ready');
+		// for backward compatibility
+		redirect_to(url_with_referer_for('account', 'edit'));
 	}
 } else {
 	Flash::set('OpenID field is empty');
