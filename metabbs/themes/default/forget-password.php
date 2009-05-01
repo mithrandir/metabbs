fe2dd4ee0fe67ab5e1583da235150941901aab51
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-05-01 05:00:27 +0000

    javascript 제거 및 오류메세지 추가

--- metabbs/themes/default/forget-password.php
+++ metabbs/themes/default/forget-password.php
@@ -4,13 +4,13 @@
 <?=error_message_box($error_messages)?>
 
 <? if (!isset($user) || !$user->get_attribute('pwresetcode', false)) { ?>
-<form method="post" action="" id="forget-password-form" onsubmit="if ($('user').value.length == 0 || $('name').value.length == 0 ) { alert('아이디와 이름을 입력해주세요.'); return false; }">
+<form method="post" action="" id="forget-password-form">
 <p>
-	<label><?=i('User ID')?><span class="star">*</span></label>
+	<label for="user" class="field"><?=i('User ID')?><span class="star">*</span></label>
 	<input type="text" name="user" id="user" value="<?=isset($params['user'])?$params['user']:''?>" class="<?=marked_by_error_message('user', $error_messages)?>"/>
 </p>
 <p>
-	<label><?=i('Screen name')?><span class="star">*</span></label>
+	<label for="name" class="field"><?=i('Screen name')?><span class="star">*</span></label>
 	<input type="text" name="name" id="name" value="<?=isset($params['name'])?$params['name']:''?>" class="<?=marked_by_error_message('name', $error_messages)?>"/>
 </p>
 <p><input type="submit" value="<?=i('Reset Password')?>" class="button"/> 
