02c6f8e8e91e8fb93ad262aebb4ec9a67069629d
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-05-15 12:50:42 +0000

    코멘트 validate 오류 수정

--- app/default/controllers/board/comment.php
+++ app/default/controllers/board/comment.php
@@ -1,18 +1,15 @@
 <?php
 permission_required('comment', $post);
 
-if (!isset($_POST['comment'])) {
-	$_POST['comment'] = array(
-		'name' => @$_POST['author'],
-		'password' => @$_POST['password'],
-		'body' => $_POST['body']
+if (!isset($params['comment'])) {
+	$params['comment'] = array(
+		'name' => @$params['author'],
+		'password' => @$params['password'],
+		'body' => $params['body']
 	);
 }
 
-$comment = new Comment($_POST['comment']);
-if (!$comment->valid()) {
-	exit;
-}
+$comment = new Comment($params['comment']);
 $comment->user_id = $account->id;
 if (!$account->is_guest()) {
 	$comment->name = $account->name;
@@ -22,15 +19,15 @@ if (!$account->is_guest()) {
 $comment->post_id = $post->id;
 
 apply_filters('PostComment', $comment, array('reply' => false));
-apply_filters('ValidateCommentCreate', $_POST, $error_messages);
+apply_filters('ValidateCommentCreate', $params, $error_messages);
 
-if (empty($post->name))
+if (empty($comment->name))
 	$error_messages->add('Please enter the name', 'author');
 
-if (empty($post->body))
+if (empty($comment->body))
 	$error_messages->add('Please enter the body', 'body');
 
-if ($account->is_guest() && strlen($post->password) < 5)
+if ($account->is_guest() && strlen($comment->password) < 5)
 		$error_messages->add('Password length must be longer than 5', 'password');
 
 if(!$error_messages->exists()) {
