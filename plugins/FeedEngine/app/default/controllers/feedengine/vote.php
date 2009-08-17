d3d088136b6f66fe7d020114d3dae6775796e12b
yupmin <yupmin@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-08-17 02:23:21 +0000

    #240 투표 추가

---
+++ plugins/FeedEngine/app/default/controllers/feedengine/vote.php
@@ -0,0 +1,27 @@
+<?php
+$post = Post::find($params['id']);
+$board = $post->get_board();
+
+if($board->exists() && $post->exists() && $board->get_attribute('feed-at-board')) {
+	// backward compatibility; #156
+	if (cookie_is_registered('seen_posts')) {
+		$seen_posts = explode(',', cookie_get('seen_posts'));
+		$_SESSION['seen_posts'] = $seen_posts;
+		cookie_unregister('seen_posts');
+	}
+
+	if (!session_is_registered('seen_posts')) {
+		$_SESSION['seen_posts'] = array();
+	}
+
+	if (!in_array($post->id, $_SESSION['seen_posts'])) {
+		$post->update_view_count(5);
+		$_SESSION['seen_posts'][] = $post->id;
+	}
+
+}
+if (!is_xhr()) {
+	redirect_back();
+}
+exit;
+?>
