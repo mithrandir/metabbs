d9751c2273acdbf3f2be3f0dc4a236233ebf743a
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2009-12-31 07:14:21 +0000

    Category 클래스 코드 정리 (주로 Query API를 쓰도록 변경)

--- metabbs/app/models/category.php
+++ metabbs/app/models/category.php
@@ -17,54 +17,56 @@ class Category extends Model {
 	function get_board() {
 		return find('board', $this->board_id);
 	}
-	
 	function get_post_count() {
-		return $this->db->fetchone("SELECT COUNT(*) FROM $this->post_table WHERE category_id=$this->id");
+		return count_all('post', 'category_id='.$this->id);
 	}
 	
 	function delete() {
-		$this->db->execute("UPDATE $this->post_table SET category_id=0 WHERE category_id=$this->id");
+		update_all('post', array('category_id' => 0), "category_id=$this->id");
 		Model::delete();
 		$this->reset_position();
 	}
 
 	function reset_position() {
 		$board = $this->get_board();
-		$categories = $this->db->fetchall("SELECT id FROM $this->table WHERE board_id={$board->id} ORDER BY position ASC");
+		$categories = $board->get_categories();
 		$position = 1;
-		foreach($categories AS $category) {
-			$category->db->execute("UPDATE $this->table SET position={$position} WHERE id={$category->id}");
+		foreach ($categories as $category) {
+			if ($category->position != $position)
+				update_all('category', array('position' => $position), 'id=' . $category->id);
+			if ($category->id == $this->id)
+				$this->position = $position;
 			$position++;
 		}
 	}
 
 	function is_first() {
-		$board = $this->get_board();
-		$higher = $this->db->fetchrow("SELECT id FROM $this->table WHERE board_id = {$board->id} AND position <> 0 AND position < {$this->position} ORDER BY position DESC LIMIT 1");
-		return !$higher->exists();
+		return $this->position == $this->db->fetchone("SELECT MIN(position) FROM $this->table WHERE board_id={$this->board_id} AND position != 0");
 	}
 
 	function is_last() {
-		$board = $this->get_board();
-		$lower = $this->db->fetchrow("SELECT id FROM $this->table WHERE board_id = {$board->id} AND position <> 0 AND position > {$this->position} ORDER BY position ASC LIMIT 1");
-		return !$lower->exists();
+		return $this->position == $this->db->fetchone("SELECT MAX(position) FROM $this->table WHERE board_id={$this->board_id} AND position != 0");
 	}
 
 	function move_lower() {
-		$board = $this->get_board();
-		$lower = $this->db->fetchrow("SELECT id, position FROM $this->table WHERE board_id = {$board->id} AND position <> 0 AND position > {$this->position} ORDER BY position ASC LIMIT 1");
-		if(!$lower->exists()) return false;
-		$this->db->execute("UPDATE $this->table SET position={$this->position} WHERE id={$lower->id}");
-		$this->db->execute("UPDATE $this->table SET position={$lower->position} WHERE id={$this->id}");
+		if ($this->is_last())
+			return false;
+
+		$this->reset_position(); // ensure that positions are unique and sequential
+		update_all('category', array('position' => $this->position), "board_id=$this->board_id AND position=".($this->position + 1));
+		update_all('category', array('position' => $this->position + 1), 'id=' . $this->id);
+		$this->position++;
 		return true;
 	}
 
 	function move_higher() {
-		$board = $this->get_board();
-		$higher = $this->db->fetchrow("SELECT id, position FROM $this->table WHERE board_id = {$board->id} AND position <> 0 AND position < {$this->position} ORDER BY position DESC LIMIT 1");
-		if(!$higher->exists()) return false;
-		$this->db->execute("UPDATE $this->table SET position={$this->position} WHERE id={$higher->id}");
-		$this->db->execute("UPDATE $this->table SET position={$higher->position} WHERE id={$this->id}");
+		if ($this->is_first())
+			return false;
+
+		$this->reset_position(); // ensure that positions are unique and sequential
+		update_all('category', array('position' => $this->position), "board_id=$this->board_id AND position=".($this->position - 1));
+		update_all('category', array('position' => $this->position - 1), 'id=' . $this->id);
+		$this->position--;
 		return true;
 	}
 }
