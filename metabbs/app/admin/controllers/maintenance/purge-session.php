cd91c96a4041f6b029547c867aad4166e9a42a1f
ditto <ditto@ebfee1e4-1e01-0410-80c6-ae36589ba299>
2010-01-10 13:41:46 +0000

    #251 처리

--- metabbs/app/admin/controllers/maintenance/purge-session.php
+++ metabbs/app/admin/controllers/maintenance/purge-session.php
@@ -1,10 +1,11 @@
 <?php
 if (is_post()) {
 	check_form_token();
+	$sid = 'sess_'.session_id(); // current session ID
 	$count = 0;
 	$d = opendir('data/session');
 	while ($f = readdir($d)) {
-		if ($f[0] == '.') continue;
+		if ($f[0] == '.' || $f == $sid) continue;
 		$path = 'data/session/'.$f;
 		$c = file_get_contents($path);
 		if (!preg_match('/seen_posts/', $c)) {
